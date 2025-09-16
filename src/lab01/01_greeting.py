5b2577ab735f899accbd279246a56bdc858086a6
Амалия Либенсон <amaliya.libenson@gmail.com>
2025-09-16 19:38:51 +0300

    Second

--- src/lab01/01_greeting.py
+++ src/lab01/01_greeting.py
@@ -1,3 +1,3 @@
 name = str(input('Имя: '))
-age = str(input('Возраст: '))
-print('Привет, '+name+'! Через год тебе будет '+age+'.')
+age = int(input('Возраст: '))
+print('Привет, '+name+'! Через год тебе будет '+str(age+1)+'.')
